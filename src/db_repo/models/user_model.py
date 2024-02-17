727ac6d898ed8ddf34c1d511e347aa90226a3913
kuronikushini <kuronikushini@aviacod.ru>
2024-02-18 01:02:58 +0700

    Add more columns cls in all models

--- src/db_repo/models/user_model.py
+++ src/db_repo/models/user_model.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, Text, String
 from sqlalchemy.orm import Mapped
 
 from .base import BaseModel, TimestampedModelMixin
@@ -9,15 +9,35 @@ from .base import BaseModel, TimestampedModelMixin
 class UserModel(BaseModel, TimestampedModelMixin):
     __tablename__ = 'user'
 
-    firstname: Mapped[str]
-    lastname: Mapped[str]
-    description: Mapped[str | None]
-    birthday: Mapped[datetime.datetime]
+    firstname: Mapped[str] = Column(
+        String(100),
+    )
+    lastname: Mapped[str] = Column(
+        String(100),
+    )
+    description: Mapped[str | None] = Column(
+        Text,
+        nullable=True,
+        default=None
+    )
+    birthday: Mapped[datetime.datetime] = Column(
+        DateTime,
+        nullable=True,
+    )
 
-    profile_link: Mapped[str]
-    phone: Mapped[str]
+    profile_link: Mapped[str] = Column(
+        String(42),
+        nullable=True,
+        default=None
+    )
+    phone: Mapped[str] = Column(
+        String(11),
+        nullable=True,
+        default=None
+    )
 
     deleted_at: Mapped[datetime.datetime | None] = Column(
         DateTime,
-        nullable=True
+        nullable=True,
+        default=None
     )
